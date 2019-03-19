9fa10df68b180dd53dd53937d2760f3b50f65699
takasiro <41959070+takasiro@users.noreply.github.com>
2019-03-19 17:40:00 +0900

    [update]光線がゴールに当たると止まるように修正
    tagがgoalのものに当たると止まります

---
+++ Assets/ParticleCheck.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCheck : MonoBehaviour
+{
+    private ParticleSystem particle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnParticleCollision(GameObject obj)
+    {
+
+        if (obj.tag == "Goal")
+        {
+            Debug.Log("衝突");
+            particle.Pause();
+  
+        }
+
+    }
+
+}

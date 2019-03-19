ee93c7889cb3a75407bd2fbf69192a461820ec18
ishikrua <ishikura.yda@gmail.com>
2019-03-19 17:17:35 +0900

    [add] 鏡の選択と配置
    クリックしたゲームオブジェクトが取得できている
    配置はまだできていません

---
+++ Assets/Ishikura/Script/Copyclick.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Copyclick : MonoBehaviour
+{
+
+    GameObject clickedGameObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            clickedGameObject = null;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            if (hit2d)
+            {
+                clickedGameObject = hit2d.transform.gameObject;
+            }
+
+            Debug.Log(clickedGameObject);
+        }
+    }
+
+    /*public void OnClick()
+    {
+        Debug.Log("あいうえお");
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            clickedGameObject = null;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            if (hit2d)
+            {
+                clickedGameObject = hit2d.transform.gameObject;
+            }
+
+            Debug.Log(clickedGameObject);
+        }
+    }
+    */
+}

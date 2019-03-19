db3cd886e22524a2b5b8ace40fe571d0f281bcca
hell25mk <hell25mk@gmail.com>
2019-03-19 14:33:31 +0900

    [add] シーンの移動スクリプトの追加
    シーンを移動するためのスクリプトとそれ用のデバッグシーンを追加

---
+++ Assets/Nagashima/SceneMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMove : MonoBehaviour
+{
+
+    public enum SceneType {
+        Scene_Title = 0,
+        Scene_Game,
+        Scene_Result,
+    }
+
+    [SerializeField]
+    private SceneType moveToScene = SceneType.Scene_Title;
+
+    private List<string> sceneList = new List<string>{
+        "TitleScene",
+        "GameScene",
+        "ResultScene",
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick() {
+
+        SceneManager.LoadScene(sceneList[(int)moveToScene]);
+
+    }
+    
+    
+
+}

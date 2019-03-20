33bc3cb4b8b618f162a35c54b1f2c34b99d50024
hell25mk <hell25mk@gmail.com>
2019-03-20 09:15:35 +0900

    [add] 妖精さん
    妖精さんの変更内容
    鏡の反射角を変更
    クリックで鏡の角度変更
    等々

---
+++ Assets/Nagashima/Script/MirrorObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorObject : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnLeftMouseClick() {
+        Debug.Log("クリックされたよ");
+        transform.Rotate(new Vector3(0, 0, 90));
+    }
+
+}
